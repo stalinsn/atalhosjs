1e325fe79b116744a0ab87c6e8f2b8658cd2472b
Stalin Nunes <stalinsn@hotmail.com>
2019-10-21 13:31:53 -0300

    Atualizacao Ler Url

--- ler_url.js
+++ ler_url.js
@@ -1,5 +1,8 @@
-var url_atual = window.location.href;
-url_atual.split('/')[3].slice(11,14);
-if(url_atual == '456'){
-    $("p.flag.black-friday-2019").css( "display","block");
-}
+$(document).ready(function(){
+	setTimeout(function(){
+		var url_atual = window.location.href.split('/')[3].slice(11,14);
+		if(url_atual == '456'){
+			$("p.flag.black-friday-2019").css( "display","block");
+		}
+	}, 300);
+});
