18c30453cc91f6b23d0c4f14dc45ec1a8612f9a6
Stalin Nunes <stalinsn@hotmail.com>
2019-10-29 10:46:56 -0300

    adicionado script para movimentar elementos do DOM

---
+++ movimenta_elemento.js
@@ -0,0 +1,5 @@
+$(window).load(function() {    
+    if (window.location.href.indexOf("/diretorio") == -1 || window.location.href.indexOf("/diretorio_2") == -1) { //verifica se o link pertence a esse diretorio ou extensao
+        $(".classe-pai-1.classe-pai-2 .classe-filho-1").append($(".classe-div-receptora")); //primeiro de onde vem e depois para onde vai   
+    }
+})
