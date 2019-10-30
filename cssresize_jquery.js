7d36cb163b9b7e9ebb515b8ae4e8fd15a1b4a713
Stalin Nunes <stalinsn@hotmail.com>
2019-10-30 18:52:59 -0300

    correçãod a função resize

--- cssresize_jquery.js
+++ cssresize_jquery.js
@@ -1,7 +1,7 @@
 $(window).resize(function(){
     if (window.matchMedia('(max-width: 768px)').matches){
-        $('.home .btn.w-qty a:nth-child(2)').css('color','redr')
+        $('.home .btn.w-qty a:nth-child(2)').css('text-align','center')
     }else{
-		 $('.home .btn.w-qty a:nth-child(2)').css('color','blue')
+		 $('.home .btn.w-qty a:nth-child(2)').css('text-align','right')
 	}
 });
